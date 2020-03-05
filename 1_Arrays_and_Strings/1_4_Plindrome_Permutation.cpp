572eec170c882a5dd6cde2fe0cb1c8a4fd5da30e
Daisuke Kasuga <begnjamin.at.mac@gmail.com>
2020-03-05 23:17:04 +0900

    new commit

---
+++ 1_Arrays_and_Strings/1_4_Plindrome_Permutation.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrindrome(string s)
+{
+    map<char, int> cnt;
+    for (char c : s) {
+        if (isspace(c)) {
+            continue;
+        }
+        if (isupper(c))
+            c = tolower(c);
+        if (!cnt.count(c))
+            cnt[c] = 1;
+        else {
+            cnt[c]++;
+        }
+    }
+
+    int num_odd = 0;
+    for (auto [key, value] : cnt) {
+        if (value % 2 == 1) {
+            num_odd++;
+        }
+    }
+
+    if (num_odd <= 1)
+        return true;
+    else {
+        return false;
+    }
+}
+
+int main()
+{
+    string s;
+    getline(cin, s);
+    if (isPrindrome(s))
+        cout << "Yes" << endl;
+    else {
+        cout << "No" << endl;
+    }
+}
