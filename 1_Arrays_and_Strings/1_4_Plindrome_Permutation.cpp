cb9fdee53ac49def0da9c2cecdd99248b8565d85
dkasuga <benjamin.at.mac@gmail.com>
2020-03-05 23:54:16 +0900

    new commit

--- 1_Arrays_and_Strings/1_4_Plindrome_Permutation.cpp
+++ 1_Arrays_and_Strings/1_4_Plindrome_Permutation.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isPrindrome(string s)
+bool isPrindrome1(string s)
 {
     map<char, int> cnt;
     for (char c : s) {
@@ -31,12 +31,53 @@ bool isPrindrome(string s)
     }
 }
 
+// change i bit of number
+int toggle(int bit_vector, int index)
+{
+    if (index < 0)
+        return bit_vector;
+
+    int mask = 1 << index;
+    if ((bit_vector & mask) == 0) {
+        bit_vector |= mask;
+    } else {
+        bit_vector &= ~mask;
+    }
+    return bit_vector;
+}
+
+int createBitVector(string s)
+{
+    int bit_vector = 0;
+    for (char c : s) {
+        if (isspace(c))
+            continue;
+        if (isupper(c))
+            c = tolower(c);
+        int x = c - 'a';
+        bit_vector = toggle(bit_vector, x);
+    }
+    return bit_vector;
+}
+
+// 整数値から1減算したものとと元の数のANDをとり，1bitだけが1になっているかどうかをチェック
+bool checkExactlyOneBitSet(int bit_vector)
+{
+    return (bit_vector & (bit_vector - 1)) == 0;
+}
+
+bool isPrindrome2(string s)
+{
+    int bit_vector = createBitVector(s);
+    return bit_vector == 0 || checkExactlyOneBitSet(bit_vector);
+}
+
 int main()
 {
     string s;
     // 空白無視して読み込む
     getline(cin, s);
-    if (isPrindrome(s))
+    if (isPrindrome2(s))
         cout << "Yes" << endl;
     else {
         cout << "No" << endl;
